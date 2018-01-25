fc625dcc9676b01c74ed83b4c84ca5ee19cec4bf
bambusek7 <bambusek7@gmail.com>
2018-01-25 23:48:20 +0100

    ide v8

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-function guardy(obj) {
+function guardy(obj, defaulty = null) {
     return new Proxy(obj, {
         get: function (target, name) {
             var t = target[name];
             if (typeof t !== Object && t !== undefined) {
                 return t;
             }
-            return guardy(t || {});
-        },
-
-        // set: function (target, property, value) {
-        //     target[property] = value;
-        // },
-
-        isEmpty: function () {
-            
-        },
-
-        original: function () {
-
+            if (t !== undefined) {
+                t.value = t;
+            }
+            return guardy(t || { __isEmpty: true, value: defaulty });
         }
     });
 }
