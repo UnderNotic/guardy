9ca75590017601d3aa57442bfa5fdee12780b5f5
undernotic <undernotic@gmail.com>
2018-01-26 12:46:56 +0100

    basic test setup, idea vnext

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-function guardy(obj, defaulty = null) {
+function guardys(obj) {
     return new Proxy(obj, {
         get: function (target, name) {
             var t = target[name];
-            if (typeof t !== Object && t !== undefined) {
+
+            if (typeof t !== "object" && t !== undefined && t !== null) {
                 return t;
             }
-            if (t !== undefined) {
-                t.value = t;
+
+            return guardys(t || {});
+}
+    });
+}
+
+function guardy(obj, defaulty) {
+    return new Proxy(obj, {
+        get: function (target, name) {
+            if (name === "__value__") {
+                return target.__value__;
             }
-            return guardy(t || { __isEmpty: true, value: defaulty });
+
+            var t = target[name];
+            if (typeof t !== "object" && t !== undefined && t !== null) {
+                return t;
+            }
+            if (typeof t === "object" && t !== defaulty) {
+                t.__value__ = t || defaulty;
+            }
+
+            return guardy(t || { __exists__: false, __value__: defaulty });
         }
     });
 }
 
+
+let x = {
+    name: {
+        first: "Peter"
+    }
+};
+
+var x2 = guardy(x).name.first.a.b;
+console.log(x2);
